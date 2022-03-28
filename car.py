5ebcba9482a62fb12b26c26d18bab65d0307c3ee
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2022-03-28 11:44:15 +0400

    car class is ready

---
+++ car.py
@@ -0,0 +1,39 @@
+import time
+from turtle import Turtle
+import random
+
+from score_board import TOP_POSITION, RIGHT_POSITION, LEFT_POSITION, BOTTOM_POSITION
+
+
+class Car(Turtle):
+    def __init__(self, y_pos):
+        super().__init__()
+        self.hideturtle()
+        self.shape("square")
+        self.shapesize(stretch_wid=1, stretch_len=3)
+        self.penup()
+        self.set_random_color()
+        self.set_x_random_position()
+        self.sety(y=y_pos)
+        self.speed = 1
+        self.showturtle()
+
+    def set_x_random_position(self):
+        x_pos = random.randrange(LEFT_POSITION, RIGHT_POSITION, 10)
+        self.setposition(x=x_pos, y=self.ycor())
+
+    def start_again(self):
+        self.showturtle()
+        self.goto(x=RIGHT_POSITION + 10, y=self.ycor())
+
+    def set_random_color(self):
+        R = random.randrange(0, 255, 10)
+        G = random.randrange(0, 255, 10)
+        B = random.randrange(0, 255, 10)
+        self.color((R, G, B))
+
+    def move(self):
+        self.setx(self.xcor() - self.speed)
+        if self.xcor() == LEFT_POSITION - 10:
+            self.hideturtle()
+            self.start_again()
--- main.py
+++ main.py
@@ -14,3 +14,28 @@ of the screen
 7) if turtle can cross the street it appearing again but now
 cars are moving faster
 """
+import time
+from turtle import Screen
+from car import Car
+from score_board import SCREEN_WIDTH, SCREEN_HEIGHT, BOTTOM_POSITION, TOP_POSITION
+
+screen = Screen()
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.title(titlestring="Turtle crossing street")
+screen.tracer(0)
+screen.colormode(255)
+game_is_on = True
+cars_array = [Car(BOTTOM_POSITION + 60)]
+last_car_y = cars_array[-1].ycor()
+while last_car_y < TOP_POSITION-60:
+    new_car = Car(last_car_y+40)
+    cars_array.append(new_car)
+    last_car_y = cars_array[-1].ycor()
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    for car in cars_array:
+        car.move()
+
+screen.exitonclick()
