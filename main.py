cb388c26b63db0b591f6c968de099d975aa8971f
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2022-03-28 10:32:13 +0400

    Started files

---
+++ main.py
@@ -0,0 +1,16 @@
+# Day 23
+"""
+The idea of the game:
+user with arrow move turtle and turtle should cross
+the street where a lot of cars
+1) create game screen
+2) create car class
+3) create car objects which are moving in one speed
+4) car objects appear in random position on the left side of
+screen and should go to right side and disappear
+5) create turtle class which is appearing in the middle bottom
+of the screen
+6) if turtle touch the car game is over
+7) if turtle can cross the street it appearing again but now
+cars are moving faster
+"""
